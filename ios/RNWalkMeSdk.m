02e2343d905e76282d9c4b5b29afd86fc5728595
Or Elmaliah <orxelm@gmail.com>
2019-10-27 11:11:31 +0200

    ios

--- ios/RNWalkMeSdk.m
+++ ios/RNWalkMeSdk.m
@@ -1,5 +1,6 @@
 
 #import "RNWalkMeSdk.h"
+#import <WalkMeSDK/WalkMeSDK.h>
 
 @implementation RNWalkMeSdk
 
@@ -8,6 +9,21 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(start:(NSString *)key secret:(NSString *)secret
+                  callback:(RCTResponseSenderBlock)callback)
+{
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (key == nil || secret == nil) {
+            callback(@[@"key or secret should not be undefined"]);
+        }
+        else {
+            [ABBI start:key withSecretKey:secret];
+            callback(@[[NSNull null]]);
+        }
+    });
+}
 
 @end
   
