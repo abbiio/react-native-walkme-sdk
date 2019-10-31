b08882d1e31c6b8eda8096895deccc63120ccf72
Or Elmaliah <orxelm@gmail.com>
2019-10-31 10:47:47 +0200

    missing import

--- android/src/main/java/com/walkme/RNWalkMeSdkModule.java
+++ android/src/main/java/com/walkme/RNWalkMeSdkModule.java
@@ -5,6 +5,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import abbi.io.abbisdk.ABBI;
 
 public class RNWalkMeSdkModule extends ReactContextBaseJavaModule {
 
